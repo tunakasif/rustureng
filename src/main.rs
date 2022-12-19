2dcc3a6199a1eaa8dcd87f012d6a0cefdcbebd90
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2022-12-19 21:31:16 +0300

    feat: add basic reqwest json with tokio
    Add basic configuration for a GET request with `reqwest`, `serde` and
    `tokio` crates.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
     println!("Hello, world!");
+    const URL: &str = "https://jsonplaceholder.typicode.com/todos?userId=1";
+    let res = reqwest::get(URL).await?.text().await?;
+    println!("Response: {:#?}", res);
+    Ok(())
 }
