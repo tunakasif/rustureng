7ba60a4d2a57c6132e949fab5328bc37bd29843b
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-01-02 01:08:36 +0300

    fix: forbidden 403 problem
    The GET request was returning 403 forbidden due to base URL of
    `www.tureng.com`. Once it is simplified to `tureng.com`, the response
    changed to 200.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-const URL: &str = "https://tureng.com/en/turkish-english/kedi";
-const USER_AGENT: &str =
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0";
+const WORD: &str = "telefon";
+const BASE_URL: &str = "https://tureng.com/en/turkish-english/";
+const USER_AGENT: &str = "MyAgent";
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let client = reqwest::Client::builder()
-        .cookie_store(true)
-        .user_agent(USER_AGENT)
-        .build()?;
+    let client = reqwest::Client::new();
+    let url = format!("{}{}", BASE_URL, WORD);
 
-    let res = client.get(URL).send().await?;
-    println!("Status: {}", res.status());
+    let res = client
+        .get(url)
+        .header("User-Agent", USER_AGENT)
+        .send()
+        .await?;
+
+    println!("Status: {:#?}", res.status());
 
     Ok(())
 }
