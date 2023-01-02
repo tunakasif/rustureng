624d9a295a0e7457be41638014ad0446301c747d
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-01-02 19:56:22 +0300

    feat: export to functions
    [WIP] Modularize code for better usage.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
+use reqwest::{header::HeaderMap, header::USER_AGENT, Client, Response};
+
 const WORD: &str = "telefon";
 const BASE_URL: &str = "https://tureng.com/en/turkish-english/";
-const USER_AGENT: &str = "MyAgent";
+const MY_USER_AGENT: &str = "MyAgent";
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let client = reqwest::Client::new();
-    let url = format!("{}{}", BASE_URL, WORD);
+    let url: String = format!("{}{}", BASE_URL, WORD);
+    let mut header_map: HeaderMap = HeaderMap::new();
+    header_map.insert(USER_AGENT, MY_USER_AGENT.parse().unwrap());
 
-    let res = client
-        .get(url)
-        .header("User-Agent", USER_AGENT)
-        .send()
-        .await?;
-
-    println!("Status: {:#?}", res.status());
-
+    let response: Response = get_response(&url, header_map).await?;
+    println!("Status: {:#?}", response.status());
     Ok(())
+}
+
+async fn get_response(
+    url: &str,
+    header_map: reqwest::header::HeaderMap,
+) -> Result<reqwest::Response, reqwest::Error> {
+    Client::new().get(url).headers(header_map).send().await
 }
