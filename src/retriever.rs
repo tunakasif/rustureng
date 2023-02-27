6b10c8279e92d48e7cfebc916aac5fde0896e387
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-02-27 23:12:22 +0300

    fix: remove unnecessary `closure` usage
    For error mapping unnecessary closures have been generated. These
    closures have been eliminated based on `clippy` suggestions.

--- src/retriever.rs
+++ src/retriever.rs
@@ -17,17 +17,17 @@ pub enum RetrieverError {
 
 pub async fn search_term(term: &str) -> Result<String, RetrieverError> {
     let url = format!("{BASE_URL}{term}");
-    let url = Url::parse(&url).map_err(|err| RetrieverError::UrlParse(err))?;
+    let url = Url::parse(&url).map_err(RetrieverError::UrlParse)?;
 
     let mut response = Request::builder()
         .method("GET")
         .uri(url.as_str())
         .header("User-Agent", MY_USER_AGENT)
         .body(())
-        .map_err(|err| RetrieverError::ChttpBuilder(err))?
+        .map_err(RetrieverError::ChttpBuilder)?
         .send_async()
         .await
-        .map_err(|err| RetrieverError::ChttpResponse(err))?;
+        .map_err(RetrieverError::ChttpResponse)?;
 
     match response.status() {
         StatusCode::OK => response
