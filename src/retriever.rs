9ce536dd89586bd118f094b5305fa444ce582ec9
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-02-27 23:26:56 +0300

    test(retriever): add retrieval `ok` test
    Add a simple test to retriever module to check if the request response
    is 200 Ok.

--- src/retriever.rs
+++ src/retriever.rs
@@ -37,3 +37,26 @@ pub async fn search_term(term: &str) -> Result<String, RetrieverError> {
         other => Err(RetrieverError::ResponseNotOk(other)),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_TERMS: [&str; 7] = [
+        "gönderi kutusu",     // valid multi-word turkish with turkish character
+        "mobile phone",        // valid multi-word english
+        "sağır",             // valid single word turkish with turkish character
+        "mailbox",             // valid single word english
+        "asda",                // suggestion single word
+        "asdfsdfafdasfdafd",   // invalid single word
+        "asdf sdfafdasfd afd", // invalid single word
+    ];
+
+    #[tokio::test]
+    async fn test_term_retrieval_is_ok() {
+        for term in TEST_TERMS {
+            let result = search_term(term).await;
+            assert!(result.is_ok());
+        }
+    }
+}
