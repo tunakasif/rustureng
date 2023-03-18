ccec08a32f62b8ae9b55e41098d5870f09a2c7fb
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-03-18 16:34:52 +0300

    feat: initialize better parser
    To address the enhancement request provided in issue #3, a better
    approach trials has been started for the HTML table parser.
    
    Refs: #3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use rustureng::parser::parse_html_content;
+use scraper::{Html, Selector};
+// use rustureng::parser::parse_html_content;
 use rustureng::retriever::{self, RetrieverError};
 // use rustureng::retriever_reqwest::{self as retriever, RetrieverError};
 use std::{env, io::Write};
 
-const WORD: &str = "telefon";
+const WORD: &str = "test";
 const WRITE_TO_FILE: bool = false;
 
 #[tokio::main]
@@ -18,9 +19,28 @@ async fn main() -> Result<(), RetrieverError> {
     if WRITE_TO_FILE {
         save_string_to_file("content.html", &content).await;
     }
-    let translation_result = parse_html_content(&content).await;
-    println!("{translation_result:#?}");
+
+    let translation_result = parse(&content).await;
+    println!("{:#?}", translation_result.len());
     Ok(())
+}
+
+async fn parse(content: &str) -> Vec<Vec<String>> {
+    let document = Html::parse_document(content);
+    let table_selector = Selector::parse("table").unwrap();
+    let tr_selector = Selector::parse("tr").unwrap();
+    let td_with_a_selector = Selector::parse("td > a").unwrap();
+
+    let tables = document.select(&table_selector);
+    tables
+        .map(|table| {
+            table
+                .select(&tr_selector)
+                .filter(|tr| tr.select(&td_with_a_selector).count() > 0)
+                .map(|tr| tr.text().collect::<Vec<_>>().join(""))
+                .collect::<Vec<String>>()
+        })
+        .collect::<Vec<_>>()
 }
 
 async fn save_string_to_file(file_name: &str, content: &str) {
