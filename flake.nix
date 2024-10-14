06d570e0890acb799c94d9db136f7198cc7a5d42
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2024-10-14 13:59:21 +0300

    build(nix): add nix configuration with `cargo2nix`
    Generate automated `Cargo.nix` file with `nix run github:cargo2nix/cargo2nix`
    command, and add `flake.nix` to use it.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs =
+    inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.75.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in
+      rec {
+        packages = {
+          # replace rustureng with your package name
+          rustureng = (rustPkgs.workspace.rustureng { });
+          default = packages.rustureng;
+        };
+      }
+    );
+}
